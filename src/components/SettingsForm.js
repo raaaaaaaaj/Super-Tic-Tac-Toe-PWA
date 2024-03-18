85de7a46031d05588f58875f21ebe470cd44b639
Raj Santosh Mhatre <5021131@fcrit.onmicrosoft.com>
2024-03-18 21:25:37 +0530

    few changes

--- src/components/SettingsForm.js
+++ src/components/SettingsForm.js
@@ -1,17 +1,8 @@
 import React from 'react';
 
 export default class SettingsForm extends React.Component {
-    state = {
-        boardSize: this.props.defaultValues.boardSize
-    };
-
-    handleChange = (event) => {
-        const { target: { name, type, value, checked } } = event;
-        this.setState({ [name]: type === 'checkbox' ? checked : value });
-    }
-
     handleSubmit = (event) => {
-        this.props.submitCallback(this.state.boardSize);
+        this.props.submitCallback(3); // board size is always 3
         event.preventDefault();
     }
 
@@ -19,15 +10,6 @@ export default class SettingsForm extends React.Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <input type="submit" value="New game" />
-                <label className="settings-label">
-                    Board size <input
-                        name="boardSize"
-                        type="number"
-                        min="2"
-                        max="10"
-                        value={this.state.boardSize}
-                        onChange={this.handleChange} />
-                </label>
             </form>
         );
     }
